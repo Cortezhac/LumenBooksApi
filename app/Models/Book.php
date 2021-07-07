b874928f6c5646d2776944779080eb2d2c58bff8
Irvin Cortez <irvincotez67@gmail.com>
2021-07-07 14:51:41 -0600

    add seeder

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -3,11 +3,13 @@
 namespace App\Models;
 
 use Illuminate\Auth\Authenticatable;
+use Illuminate\Contracts\Auth\Access\Authorizable as AuthorizableContract;
+use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Laravel\Lumen\Auth\Authorizable;
 
-class Book extends Model
+class Book extends Model implements AuthenticatableContract, AuthorizableContract
 {
     use Authenticatable, Authorizable, HasFactory;
 
